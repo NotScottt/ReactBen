7fb70e6016b2aa98a5e7c245248891bf295985d5
Scott Zachariae <scott.zachariae@ibykus.de>
2025-06-04 11:28:09 +0200

    fixed assets error

--- src/functionality/PhoneRing.js
+++ src/functionality/PhoneRing.js
@@ -8,16 +8,16 @@ export function playPhoneRing() {
     }
     lastPlayed = now;
 
-    const audio = new Audio("/assets/sounds/phone.mp3");
+    const audio = new Audio("assets/sounds/phone.mp3");
     audio.play();
 
     setTimeout(() => {
-        const audio2 = new Audio("/assets/sounds/phone.mp3");
+        const audio2 = new Audio("assets/sounds/phone.mp3");
         audio2.play();
     }, 2000); // 2 Sekunden Delay
 
     setTimeout(() => {
-        const audio3 = new Audio("/assets/sounds/phone-answer.mp3");
+        const audio3 = new Audio("assets/sounds/phone-answer.mp3");
         audio3.play();
     }, 3100); // 3 Sekunden Delay
 }
