dc16372628b38e8cc60427275f04527baf05715e
Scott Zachariae <scott.zachariae@ibykus.de>
2025-06-05 13:23:38 +0200

    added ultimate Ben Skin

--- src/functionality/SkinPicker.js
+++ src/functionality/SkinPicker.js
@@ -1,6 +1,9 @@
 export function SkinPicker(rebirtLevel) {
+    // Stages
+    // 250k, 500k, 1M, 2M, 4M, 8M, 16M, 32M, 64M, 128M, 256M
+    
     const skinsSrc = [
-        'assets/BenSkins/BenFigure.png',
+        'assets/BenSkins/BenFigure.png',  
         'assets/BenSkins/EmoBen.png',
         'assets/BenSkins/LeanBen.png',
         'assets/BenSkins/PencilBen.png',
@@ -10,6 +13,7 @@ export function SkinPicker(rebirtLevel) {
         'assets/BenSkins/GoldenBen.png',
         'assets/BenSkins/DiamondBen.png',
         'assets/BenSkins/UltimateBen.png',
+        'assets/BenSkins/TheOneAndOnlyBen.png',
     ]
 
     for (let index = 0; index < skinsSrc.length; index++) {
