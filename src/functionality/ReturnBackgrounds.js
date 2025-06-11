79606f6c23b88f8aa96196bd1235907655447a31
Scott Zachariae <scott.zachariae@ibykus.de>
2025-06-11 08:29:31 +0200

    added new rooms

--- src/functionality/ReturnBackgrounds.js
+++ src/functionality/ReturnBackgrounds.js
@@ -6,6 +6,10 @@ export function ReturnBackgrounds() {
         { src: 'assets/Backgrounds/School.jpg', multiplier: 4 },
         { src: 'assets/Backgrounds/Windows.jpg', multiplier: 5 },
         { src: 'assets/Backgrounds/Wasteland.jpg', multiplier: 6 },
+        { src: 'assets/Backgrounds/Jail.jpg', multiplier: 7 },
+        { src: 'assets/Backgrounds/Park.jpg', multiplier: 8 },
+        { src: 'assets/Backgrounds/Streets.jpg', multiplier: 9 },
+        { src: 'assets/Backgrounds/Erfurt.jpg', multiplier: 10 },
     ]
 
     return RoomSrc;
